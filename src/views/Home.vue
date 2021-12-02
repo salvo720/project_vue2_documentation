302757c17707d9aaa0547b0ce600c57842c79e7c
salvo720 <salvocelano91@gmail.com>
2021-12-02 23:10:19 +0100

    continuo da documentazione

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <h4>
+    <span v-bind:title="message">
+      Passa il mouse su di me per alcuni secondi
+      per vedere il mio titolo legato dinamicamente!
+    </span>
+    <hr>
+    <br>
+    <span v-if="seen">
+      Ora mi vedi
+    </span>
+    <hr>
+    <br>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo">
+        {{ todo.text }}
+      </li>
+    </ul>
+  <br>
+  <hr>
+    <button v-on:click="reverseMessage">{{ message }}</button>
+    <hr>
+    <h2>Valore da invertire</h2>
+    {{ reverse_message }}
+    <hr>
+  <hr>
+  <br>
+   <input v-model="reverse_message">
+  <hr>
+  <h2>valore reverse message</h2>
+  {{ reverse_message }}
+  <hr>
+  </h4>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'app',
+ data() {
+    return {
+      message:'You loaded this page on ' + new Date().toLocaleString() ,
+      reverse_message : 'Reverse message' ,
+      seen: true ,
+       todos: [
+      { text: 'impara JavaScript' },
+      { text: 'impara Vue' },
+      { text: 'Build something awesome' }
+    ],
+    }
+  },
+    methods: {
+    reverseMessage: function () {
+      this.reverse_message = this.reverse_message.split('').reverse().join('')
+    }
   }
+
 }
 </script>
