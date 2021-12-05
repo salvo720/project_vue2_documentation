63e4fbd5c86d091b6c26fbea7391bb94bd2568ff
salvo720 <salvocelano91@gmail.com>
2021-12-05 20:05:06 +0100

    continuo documentazione fermo a dati e metodi vuejs documentation

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="todoitem">
+        <!-- comment here for show only element  -->
+            <hr>
+            <h4>
+                {{ show_element }}
+                {{ todo }}
+            </h4>
+            <hr>
+        <!--  -->
+        <h5>
+            {{ todo.id }} |
+            {{ todo.text }}
+        </h5>
+    </div>
+</template>
+<script>
+export default {
+    name: 'todoitem',
+    data(){
+        return{
+            show_element:'elementi completi passati al componente ',
+        }
+    },
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>',
+  
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,7 +31,9 @@
   <h2>valore reverse message</h2>
   {{ reverse_message }}
   <hr>
-  <TodoItem></TodoItem>
+  <TodoItem v-for="item in groceryList" v-bind:todo="item" v-bind:key="item.id"></TodoItem>
+  <!-- l'elemento passao al v-bind (in questo caso todo ) indica il nome della variabile a cui passeremo i dati .
+   il v-bind-key: , viene usato per scegliere la chiave del for  -->
   </h4>
 </div>
 </template>
@@ -43,20 +45,25 @@ import TodoItem from '@/components/TodoItem.vue'
 
 
 export default {
-  name: 'app',
- data() {
+name: 'app',
+  data() {
     return {
       message:'You loaded this page on ' + new Date().toLocaleString() ,
       reverse_message : 'Reverse message' ,
       seen: true ,
-       todos: [
+      todos: [
       { text: 'impara JavaScript' },
       { text: 'impara Vue' },
       { text: 'Build something awesome' }
-    ],
+      ],
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
     }
   },
-    methods: {
+  methods: {
     reverseMessage: function () {
       this.reverse_message = this.reverse_message.split('').reverse().join('')
     }
@@ -64,6 +71,5 @@ export default {
   components: {
     TodoItem // Register a new component
   },
-
 }
 </script>
