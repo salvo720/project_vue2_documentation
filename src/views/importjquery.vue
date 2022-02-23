e01c9e639b06520206e45eaffded9a3778560159
ws_27@jogroup.eu <me>
2022-02-23 10:09:00 +0100

    ripulito per richiesta axios login.vue

---
+++ src/views/importjquery.vue
@@ -0,0 +1,15 @@
+<script>
+// importazione jquery
+/* 
+import $ from "jquery";
+
+export default {
+    name: 'welcome',
+    mounted: function() {
+        window.setTimeout(function() {
+            $('.logo').css('opacity', 0);   
+        }, 1000);
+    } 
+}
+ */
+</script>
