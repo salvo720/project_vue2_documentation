b38a7bbcf7638a04952f965868120173524280c0
ws_27@jogroup.eu <me>
2022-02-25 16:48:57 +0100

    process in unico array

--- vue_cdn/vue_crud_2/process.php
+++ vue_cdn/vue_crud_2/process.php
@@ -1,41 +1,33 @@
 <?php
-
-$email = $_POST['email'];
-$password = $_POST['password'];
-$remember_me_check = $_POST['remember_me_check'];
+// $email = $_POST['email'];
+// $password = $_POST['password'];
+// $remember_me_check = $_POST['remember_me_check'];
 
 // Create connection
 $con=mysqli_connect("localhost","salvo","","vue_crud");
-if($con->connection_error){
-    die("Connection fail " .$con->connection_error);
-}else{
-    echo "connected <br>";
+ 
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+ 
+// Select all of our stocks from table 'stock_tracker'
+$sql = "SELECT * 
+        FROM  users";
 
-$result = array('error'=>false);
-$action = '';
-
-if(isset($_GET['action'])){
-    $action = $_GET['action'];
-}
-
-echo "action : ",  $action , "<br>";
-if($action == 'read'){
-    // Select all of our stocks from table 'stock_tracker'
-    $sql = "SELECT * FROM  users ";
-    echo "sql : ",$sql , "<br>";
-
-    $result = mysqli_query($con, $sql);
-    echo "result : ",$result , "<br>";
-
-    $users = array();
-    while($row = $result->fetch_assoc()){
-        array_push($users , $row );
+// echo $sql;
+ $users= array();
+if ($result = mysqli_query($con, $sql)) {
+    // Fetch one and one row
+    echo "<pre>";
+    while ($row = $result->fetch_assoc()) {
+        array_push($users , $row);
     }
-    $result['users'] = $users;
-}
+    $result_2['users'] = $users ;
+  }
 
-echo json_encode($result);
+  echo json_encode($result_2);
  
 // Close connections
 mysqli_close($con);
